62b292ddf6a6fb71097c5cfa527b625366c46a3f
leadiv <paul@leadiv.com>
2016-04-29 16:40:01 -0400

    feature: Make slide viewable based on current and order
    A slide will be viewable only if the current property matches
    the order property.

--- src/components/slide/index.js
+++ src/components/slide/index.js
@@ -3,21 +3,32 @@ import React, { Component, PropTypes } from 'react';
 require('./slide.scss');
 
 class Slide extends Component {
+
+    constructor(props) {
+        super(props);
+        this.isViewable = this.isViewable.bind(this);
+    }
+
+    isViewable() {
+        return this.props.current === +this.props.order;
+    }
+
     render() {
         return (
-            <div className="slide">
-                <header className="slide__header">
-                    <h1 className="slide__title">{this.props.title}</h1>
-                </header>
+            this.isViewable() &&
+                <div className="slide">
+                    <header className="slide__header">
+                        <h1 className="slide__title">{this.props.title}</h1>
+                    </header>
 
-                <ul>
-                    {this.props.children}
-                </ul>
+                    <ul>
+                        {this.props.children}
+                    </ul>
 
-                <footer className="slide__footer">
-                    {this.props.order} / {this.props.total}
-                </footer>
-            </div>
+                    <footer className="slide__footer">
+                        {this.props.order} / {this.props.total}
+                    </footer>
+                </div>
         );
     }
 }
