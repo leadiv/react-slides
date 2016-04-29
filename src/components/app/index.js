bab1fe4daedc92f17a62a447f12a40deafdd4b9c
leadiv <paul@leadiv.com>
2016-04-29 16:41:35 -0400

    feature: Add a second slide
    Adds state to the app to show the current slide and keep track of
    the total number slides.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -5,10 +5,23 @@ import BulletPoint from '../bullet-point';
 require('./app.scss');
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            current: 1,
+            total: 2
+        }
+    }
+
     render() {
         return (
             <div className="presentation">
-                <Slide title="Intro" order="1" total="1">
+                <Slide title="What is ReactJS?" order="1" total={this.state.total} current={this.state.current}>
+                    <BulletPoint>Hello this will be a simple slide presentation app when done.</BulletPoint>
+                    <BulletPoint>A tag is just a tag</BulletPoint>
+                </Slide>
+                <Slide title="V is for View" order="2" total={this.state.total} current={this.state.current}>
                     <BulletPoint>Hello this will be a simple slide presentation app when done.</BulletPoint>
                     <BulletPoint>A tag is just a tag</BulletPoint>
                 </Slide>
