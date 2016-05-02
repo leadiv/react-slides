edb262c59b062a8962ed869d8aefe34915d7c3f9
leadiv <paul@leadiv.com>
2016-05-01 22:35:01 -0400

    feat: Add fetching to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import slide from './slide';
+import fetching from './fetching';
 
 const rootReducers = combineReducers({
-    slide
+    slide,
+    fetching
 }); 
 
 export default rootReducers;
